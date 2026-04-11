51bebee8631814fbecc2b4387c3892f2bd055922
mchogo <chogokin.masa@gmail.com>
2026-04-11 15:58:59 +0900

    同期

--- e2e/tests/login.spec.ts
+++ e2e/tests/login.spec.ts
@@ -2,6 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('ログイン画面が表示されること', async ({ page }) => {
   await page.goto('/login');
-  await expect(page).toHaveTitle(/いとうさんフォトマネージャー/);
+  await expect(page).toHaveTitle(/フォトマネージャー/);
   await expect(page.getByRole('button', { name: 'ログイン' })).toBeVisible();
 });
